8eda911bf571bccd3409f024fb368886e0111bc0
nateyb93 <brownn15@up.edu>
2015-03-12 02:21:53 -0700

    Debug generated files; I am at the point now where lines can be drawn and the audio plays.

--- Spintervals_win/Spintervals/Spintervals/MainPage.xaml.cs
+++ Spintervals_win/Spintervals/Spintervals/MainPage.xaml.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -22,11 +25,13 @@ namespace Spintervals
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private bool _isPlaying = false;
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+
         }
 
         /// <summary>
@@ -43,6 +48,70 @@ namespace Spintervals
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+        }
+
+        /// <summary>
+        /// Draws random lines to test canvases
+        /// </summary>
+        private void DrawRandomLines()
+        {
+            Random rand = new Random();
+            for (int i = 1; i < (int)IntervalCanvas.ActualWidth; )
+            {
+                int h = rand.Next(1, (int)IntervalCanvas.ActualHeight);
+                for (int j = 0; j < 4; j++)
+                {
+                    DrawRect(i, 2, h);
+                    i += 3;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws a rectangle on the interval canvas
+        /// </summary>
+        /// <param name="x">horizontal position of rectangle</param>
+        /// <param name="y">vertical position of rectanlge</param>
+        /// <param name="w">width of rectangle</param>
+        /// <param name="h">height of rectangle</param>
+        private void DrawRect(int x, int w, int h)
+        {
+            Rectangle rect = new Rectangle();
+            Canvas.SetLeft(rect, x);
+            Canvas.SetTop(rect, IntervalCanvas.ActualHeight - h);
+            rect.Width = w;
+            rect.Height = h;
+            rect.Fill = new SolidColorBrush(Colors.Green);
+
+            IntervalCanvas.Children.Add(rect);
+        }
+
+        private void IntervalCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawRandomLines();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!_isPlaying)
+            {
+
+                Image i = new Image();
+                i.Source = new BitmapImage(new Uri("ms-appx:/Images/pause.png"));
+                PlayButton.Content = i;
+
+                _isPlaying = true;
+                songMediaElement.Play();
+            }
+            else
+            {
+                Image i = new Image();
+                i.Source = new BitmapImage(new Uri("ms-appx:/Images/play.png"));
+                PlayButton.Content = i;
+                _isPlaying = false;
+                songMediaElement.Pause();
+            }
+            
         }
     }
 }
