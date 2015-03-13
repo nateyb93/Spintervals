3f6c8d89381abdd48415208bcdf7e048f6ba78c9
nateyb93 <brownn15@up.edu>
2015-03-12 21:39:30 -0700

    Fixed layout for vertical screens and music scrolling functionality is working

--- Spintervals_win/Spintervals/Spintervals/MainPage.xaml.cs
+++ Spintervals_win/Spintervals/Spintervals/MainPage.xaml.cs
@@ -26,12 +26,39 @@ namespace Spintervals
     public sealed partial class MainPage : Page
     {
         private bool _isPlaying = false;
+
+        private DispatcherTimer _timer;
+
         public MainPage()
         {
             this.InitializeComponent();
-
             this.NavigationCacheMode = NavigationCacheMode.Required;
 
+        }
+
+        private void _initTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += _timer_Tick;
+        }
+
+        private void _timer_Tick(object sender, object e)
+        {
+            timelineSlider.Value = songMediaElement.Position.TotalSeconds;
+            PositionTextBlock.Text =
+                songMediaElement.Position.Minutes.ToString("0") + ":" + songMediaElement.Position.Seconds.ToString("00");
+        }
+
+        /// <summary>
+        /// Initializes the slider control with the 
+        /// </summary>
+        private void _initSlider()
+        {
+            timelineSlider.Maximum = songMediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+            timelineSlider.SmallChange = 1;
+            timelineSlider.LargeChange = Math.Min(10, songMediaElement.NaturalDuration.TimeSpan.Seconds / 10);
+            DurationTextBlock.Text = songMediaElement.NaturalDuration.TimeSpan.Minutes.ToString("0") + ":" + songMediaElement.NaturalDuration.TimeSpan.Seconds.ToString("00");
         }
 
         /// <summary>
@@ -58,12 +85,43 @@ namespace Spintervals
             Random rand = new Random();
             for (int i = 1; i < (int)IntervalCanvas.ActualWidth; )
             {
-                int h = rand.Next(1, (int)IntervalCanvas.ActualHeight);
-                for (int j = 0; j < 4; j++)
+                for (int k = 0; k < 5; k++)
                 {
-                    DrawRect(i, 2, h);
-                    i += 3;
+                    Color c;
+
+                    switch (k)
+                    {
+                        case 0:
+                            c = Colors.White;
+                            break;
+                        case 1:
+                            c = Colors.Red;
+                            break;
+                        case 2:
+                            c = Colors.Lime;
+                            break;
+                        case 3:
+                            c = Colors.Blue;
+                            break;
+                        case 4:
+                            c = Colors.Yellow;
+                            break;
+                        default:
+                            c = Colors.White;
+                            break;
+                    }
+
+                    int h = rand.Next(1, (int)IntervalCanvas.ActualHeight);
+                    for (int j = 0; j < (int)IntervalCanvas.ActualWidth / 5; j += 3)
+                    {
+                        if (i >= (int)IntervalCanvas.ActualWidth)
+                            break;
+                        
+                        DrawRect(i, 2, h, c);
+                        i += 3;
+                    }
                 }
+                
             }
         }
 
@@ -74,44 +132,82 @@ namespace Spintervals
         /// <param name="y">vertical position of rectanlge</param>
         /// <param name="w">width of rectangle</param>
         /// <param name="h">height of rectangle</param>
-        private void DrawRect(int x, int w, int h)
+        private void DrawRect(int x, int w, int h, Color c)
         {
             Rectangle rect = new Rectangle();
             Canvas.SetLeft(rect, x);
             Canvas.SetTop(rect, IntervalCanvas.ActualHeight - h);
             rect.Width = w;
             rect.Height = h;
-            rect.Fill = new SolidColorBrush(Colors.Green);
+            rect.Fill = new SolidColorBrush(c);
 
             IntervalCanvas.Children.Add(rect);
         }
 
+        /// <summary>
+        /// Called when the interval canvas has been initialized. We cannot draw any 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void IntervalCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            DrawRandomLines();
+
         }
 
+        /// <summary>
+        /// Called when the play button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             if(!_isPlaying)
             {
-
                 Image i = new Image();
                 i.Source = new BitmapImage(new Uri("ms-appx:/Images/pause.png"));
+                i.Stretch = Stretch.UniformToFill;
+                i.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
                 PlayButton.Content = i;
-
                 _isPlaying = true;
                 songMediaElement.Play();
+                _timer.Start();
             }
             else
             {
                 Image i = new Image();
                 i.Source = new BitmapImage(new Uri("ms-appx:/Images/play.png"));
+                i.Stretch = Stretch.UniformToFill;
+                i.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
                 PlayButton.Content = i;
                 _isPlaying = false;
                 songMediaElement.Pause();
+                _timer.Stop();
             }
             
+        }
+
+        /// <summary>
+        /// Called when the timelineSlider is dragged to a new position
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timelineSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            songMediaElement.Position = TimeSpan.FromSeconds(timelineSlider.Value);
+            PositionTextBlock.Text =
+                songMediaElement.Position.Minutes.ToString("0") + ":" + songMediaElement.Position.Seconds.ToString("00");
+        }
+
+        /// <summary>
+        /// Called when a song is opened by the media player.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void songMediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            _initSlider();
+            _initTimer();
+            DrawRandomLines();
         }
     }
 }
